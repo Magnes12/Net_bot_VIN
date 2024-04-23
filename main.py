1249075dd9539746262ba5c996c96aff72cb900b
Damian <m4gn3s@gmail.com>
2024-04-23 20:15:42 +0200

    TOMAKE

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import openpyxl
 from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,25 +13,23 @@ vin_column = sheet['A']
 auto_column = sheet['B']
 
 
-# chrome_options = Options()
-# chrome_options.add_argument("--headless") options=chrome_options
+edge_options = Options()
+edge_options.add_argument("--headless")
 
-driver = webdriver.Chrome()
+driver = webdriver.Edge(options=edge_options)
 
 for i in range(1, len(vin_column)):
     vin = vin_column[i].value
     print(vin)
     if vin:
         try:
-            driver.get(f'path')
+            driver.get(f'https://pl.wikipedia.org/wiki/{vin}')
             info = WebDriverWait(driver, 10).until(
-                EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div[3]/div[5]/div[1]/p[2]"))
-            )
+                EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/h2[1]/span[1]")))
             auto_info = info.text if info else ""
         except Exception as e:
             print(f"Error: {e}")
-            auto_info = "Not found"
-
+            auto_info = "Nie znaleziono/Błąd"
         auto_column[i].value = auto_info
 
 wb.save('cars.xlsx')
