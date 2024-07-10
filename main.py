0cab95280262ec6a6b4fcc4930ebec8e9155b68d
Damian <m4gn3s@gmail.com>
2024-07-10 15:26:13 +0200

    SM

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import time
 import getpass
 import openpyxl
 import subprocess
+
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 from contextlib import contextmanager
 
 
